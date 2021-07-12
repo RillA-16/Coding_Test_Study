10525ab91bbc506f5a4c547d533c3427bdffa51e
RillA-16 <68328998+RillA-16@users.noreply.github.com>
2021-07-12 16:49:10 +0900

    [백준] 1463 입력 값 1로 만들기

---
+++ Algorithm/src/sub/Test.java
@@ -0,0 +1,16 @@
+package sub;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[] hi = new int[4];
+
+		hi[0] = 1;
+		hi[1] = 1241;
+		hi[3] = 32;
+
+		for (int i = 0; i < 4; i++) {
+			System.out.println(hi[i]);
+		}
+	}
+}
