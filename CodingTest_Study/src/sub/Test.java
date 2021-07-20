3d131d2e759767d545e83d779738cdfb4b94e33e
RillA-16 <68328998+RillA-16@users.noreply.github.com>
2021-07-20 16:42:22 +0900

    [백준] 입력한 수 중에 소수 구하기

--- CodingTest_Study/src/sub/Test.java
+++ CodingTest_Study/src/sub/Test.java
@@ -3,14 +3,11 @@ package sub;
 public class Test {
 
 	public static void main(String[] args) {
-		int[] hi = new int[4];
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 6;
 
-		hi[0] = 1;
-		hi[1] = 1241;
-		hi[3] = 32;
-
-		for (int i = 0; i < 4; i++) {
-			System.out.println(hi[i]);
-		}
+		System.out.println(6 % 2);
 	}
 }
